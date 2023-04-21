5f5df7c30831d2c93f4b2914ced385ba0823bf4f
nickwbarber <nickwbarber@gmail.com>
2023-04-21 14:51:07 +0800

    Add total, average, and percent positive statistics

--- part01/unicafe/src/App.js
+++ part01/unicafe/src/App.js
@@ -12,11 +12,24 @@ const App = () => {
   const [ good, setGood ] = useState(0)
   const [ bad, setBad ] = useState(0)
   const [ neutral, setNeutral ] = useState(0)
-  
+
   const incGood = () => setGood(good + 1)
   const incBad = () => setBad(bad + 1)
   const incNeutral = () => setNeutral(neutral + 1)
-  
+  const calcAverage = (good, bad, neutral) => {
+    if (good === 0 && bad === 0) {
+      return 0 
+    }
+    return ((good - bad) / (good + neutral + bad))
+  }
+  const calcPercentPositive = (good, bad, neutral) => {
+    if (good === 0 && bad === 0) {
+      return 0 
+    }
+    return (good / (good + bad + neutral))
+  }
+
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -24,11 +37,14 @@ const App = () => {
       <Button label="good" handleClick={incGood}/>
       <Button label="neutral" handleClick={incNeutral}/>
       <Button label="bad" handleClick={incBad}/>
-    
+
       <h1>statistics</h1>
       <StatDisplay label="good" count={good} />
       <StatDisplay label="neutral" count={neutral} />
       <StatDisplay label="bad" count={bad} />
+      <StatDisplay label="total" count={good + bad + neutral} />
+      <StatDisplay label="average" count={calcAverage(good, bad, neutral)} />
+      <StatDisplay label="positive" count={calcPercentPositive(good, bad, neutral) + ' %'} />
     </div>
   );
 }
