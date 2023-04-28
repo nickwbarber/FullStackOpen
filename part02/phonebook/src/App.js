36fb6c32c0be15551791b28852bace62309c6423
nickwbarber <nickwbarber@gmail.com>
2023-04-28 19:04:05 +0800

    Connect frontend to backend
    Utilize `json-server` to serve the numbers

--- part02/phonebook/src/App.js
+++ part02/phonebook/src/App.js
@@ -1,28 +1,57 @@
-import defaults       from './defaults.json'
-import NewState       from './util'
+// external libraries
+import { useEffect } from 'react'
+import axios from 'axios'
+
+// defaults
+import defaults from './defaults.json'
+
+// helper functions
+import NewState from './util'
+
+// components
 import PhonebookView  from './components/PhonebookView'
 import EntryForm      from './components/EntryForm'
 import QueryForm      from './components/QueryForm'
 
+// main
 const App = () => {
   const nameState         = NewState(defaults.name)
   const personsState      = NewState(defaults.persons)
   const phonenumberState  = NewState(defaults.phonenumber)
   const queryState        = NewState(defaults.query)
 
+  useEffect(() => {
+    axios
+    .get(`${defaults.serverUrl}/persons`)
+    .then(response => {
+      personsState.setter(response.data)
+    })
+  // }, [personsState])
+  }, [])
+
   return (
     <div>
       <h2>Phonebook</h2>
       <div>
-        <EntryForm nameState={nameState} phonenumberState={phonenumberState} personsState={personsState} ></EntryForm>
+        <EntryForm
+          nameState={nameState}
+          phonenumberState={phonenumberState}
+          personsState={personsState}
+        >
+        </EntryForm>
       </div>
       <h2>Numbers</h2>
       <h3>Filter by name: </h3>
       <div>
-        <QueryForm queryState={queryState}></QueryForm>
+        <QueryForm
+          queryState={queryState}>
+        </QueryForm>
       </div>
       <div>
-        <PhonebookView persons={personsState.value} query={queryState.value}></PhonebookView>
+        <PhonebookView
+          persons={personsState.value}
+          query={queryState.value}>
+        </PhonebookView>
       </div>
     </div>
   );
